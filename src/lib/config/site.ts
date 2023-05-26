ab1c183e704343aaaeb21797abbfb88b9f005e83
MohammadBnei <mohammadamine.banaei@gmail.com>
2023-05-26 21:56:33 +0200

    feat(blog): adding microservice blog post

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,16 +2,22 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'bnei.dev',
+  title: 'Blog',
+  subtitle: 'Passionate developper - Teacher',
   lang: 'en-US',
-  description: 'Powered by SvelteKit/Urara',
+  description: `Welcome to my tech blog where I discuss technical aspects related to software development. I am a passionate developer with a deep interest in nodejs, golang, svelte, microservices, and kubernetes technologies. 
+
+  Through my blog, I share my experiences and provide insights on topics related to these technologies, such as best practices, tips, tutorials, and other developer-focused resources. My goal is to share technical knowledge and help other developers learn and grow their skills.
+  
+  In this blog, I explore the latest developments in these technologies, including updates on emerging trends, new features, and tools. Whether you are a beginner or an experienced developer, I cover topics that will help you improve your code quality, performance, and efficiency.
+  
+  Join me in my tech exploration journey to discover the exciting world of nodejs, golang, svelte, microservices, and kubernetes. Subscribe to my blog and stay up-to-date with the latest trends in the industry and learn how to become a better developer.`,
   author: {
     avatar: '/assets/maskable@512.png',
-    name: 'John Doe',
-    status: '🌸',
-    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    name: 'Mohammad-Amine BANAEI',
+    status: '🧑🏽‍💻',
+    bio: 'From Paris with love'
   },
   themeColor: '#3D4451'
 }
