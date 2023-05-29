e0008f6334dca120c45d2d540c4b9c03bfb63f35
MohammadBnei <mohammadamine.banaei@gmail.com>
2023-05-29 15:40:19 +0200

    feat(post.ts): add giscus comment system configuration The giscus comment system is now configured in the post.ts file. This allows for a more interactive and user-friendly commenting experience for readers of the blog. The configuration includes the repository name, repository ID, category ID, and various other options such as enabling reactions and setting the theme.

--- src/lib/config/post.ts
+++ src/lib/config/post.ts
@@ -1,3 +1,17 @@
 import type { PostConfig } from '$lib/types/post'
 
-export const post: PostConfig = {}
+export const post: PostConfig = {
+  comment: {
+    use: ['giscus'],
+    giscus: {
+      repo: 'mohammadbnei/blog-2023',
+      repoID: 'R_kgDOJoATxg',
+      categoryID: 'DIC_kwDOJoATxs4CW0Bo',
+      reactionsEnabled: true,
+      // input-position: 'bottom',
+      theme: 'preferred_color_scheme',
+      lang: 'en',
+      loading: 'lazy'
+    }
+  }
+}
