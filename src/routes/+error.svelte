2238a78b3bee46bab3a0810a429b56d8cd4218e6
MohammadBnei <mohammadamine.banaei@gmail.com>
2023-07-23 23:12:24 +0200

    feat(error.svelte): add error page component The error.svelte file is added, which contains the markup and logic for displaying an error page. It imports the "page" store from "$app/stores" to access the status and error message. The error page component displays a 404 image, the status code, and the error message. The 404.jpg image file is also added to the urara/assets directory. This addition allows for a custom error page to be shown when there is an error in the application.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { page } from "$app/stores"
+
+</script>
+
+<div class="min-h-screen hero bg-base-200">
+	<div class="flex-col hero-content lg:flex-row">
+		<img src="/assets/404.jpg" class="max-w-xs rounded-lg shadow-2xl lg:max-w-sm" alt="404" />
+		<div>
+			<h1 class="text-5xl font-bold">{$page.status}</h1>
+			<p class="py-6">{$page.error?.message}</p>
+		</div>
+	</div>
+</div>
