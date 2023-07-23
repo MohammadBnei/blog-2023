59699f98e12f4339c4aa30087bc938be0d66cadb
MohammadBnei <mohammadamine.banaei@gmail.com>
2023-07-23 23:22:20 +0200

    refactor(transition.svelte): add $lang to the key directive The key directive in the transition.svelte component now includes the $lang store variable. This ensures that the component is re-rendered whenever the language changes, allowing for smooth transitions between different language versions of the component.

--- src/lib/components/transition.svelte
+++ src/lib/components/transition.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+  import { lang } from '$lib/stores/lang'
   import { fly } from 'svelte/transition'
   export let path: string = ''
 </script>
 
-{#key path}
+{#key path + $lang}
   <div
     class="bg-base-100 md:bg-base-200 min-h-screen pt-16 md:pb-8 lg:pb-16"
     in:fly={{ y: 100, duration: 300, delay: 300 }}
